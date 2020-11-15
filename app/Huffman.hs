b99fe8f58f1d9320c5c130ba2c52d91fca0ed650
Philip Holler <philipholler94@icloud.com>
2020-11-15 10:38:31 +0100

    Clean up done. Finished and tested.

--- app/Huffman.hs
+++ app/Huffman.hs
@@ -4,42 +4,7 @@ import Data.Map (fromListWith)
 import Data.Map (toList)
 import Data.List (sortBy, insertBy)
 
-data HTree  = Leaf Int Char
-            | Node Int [Char] HTree HTree
-            deriving (Show)
-
-mergeTrees :: HTree -> HTree -> HTree
-mergeTrees t1 t2  = Node (nodeValue t1 + nodeValue t2) (nodeChars t1 ++ nodeChars t2) t1 t2
-
-nodeValue :: HTree -> Int
-nodeValue (Leaf v _) = v
-nodeValue (Node v _ _ _) = v
-
-nodeChars :: HTree -> [Char]
-nodeChars (Leaf _ c) = [c]
-nodeChars (Node _ cs _ _) = cs
-
-frequencies :: String -> [(Char, Int)]
-frequencies s = toList (fromListWith (+) [(c, 1) | c <- s])
-
-sortFrequencies :: [(Char, Int)] -> [(Char, Int)]
-sortFrequencies = sortBy (\ a b -> compare (snd b) (snd a))
-
-makeTable :: HTree -> [([Int], Char)]
-makeTable (Leaf int char) = [([], char)]
-makeTable (Node int cs t1 t2) = makeTableHelper t1 [0] ++ makeTableHelper t2 [1]
-
-makeTableHelper :: HTree -> [Int] -> [([Int], Char)]
-makeTableHelper (Leaf int char) pathLst = [(pathLst, char)]
-makeTableHelper (Node int cs t1 t2) pathLst = makeTableHelper t1 (pathLst ++ [0]) ++ makeTableHelper t2 (pathLst ++ [1])
-
-myHTree :: HTree
-myHTree = Node 1 "abcdefgh" (Node 2 "abc"   (Leaf 3 'a')
-                                            (Node 4 "bc"   (Leaf 5 'b') (Leaf 6 'c')))
-                            (Node 7 "defgh" (Node 8 "de"   (Leaf 9 'd') (Leaf 10 'e'))
-                                            (Node 11 "fgh" (Leaf 12 'f')
-                                                           (Node 13 "gh" (Leaf 14 'g') (Leaf 15 'h'))))
-
+-- Use this to test! First apply readInput in REPL. Then write the input string and press enter.
 readInput :: IO ()
 readInput = do
                 input <- getLine
@@ -56,86 +21,152 @@ readInput = do
                   Just decoded -> putStrLn $ "Decoded message: " ++ decoded
                 return ()
 
+-- Takes a msg with an encodingTable and returns the encoded msg
+encodeMessage :: String -> [([Char], Char)] -> String
+encodeMessage [] _ = [] -- If message is empty
+encodeMessage (char:rest) encodingTable = let encodingOfChar = lookupByValue encodingTable char -- Path as "110"
+                                          in
+                                              case encodingOfChar of
+                                                Nothing -> "Could not encode message"
+                                                Just encodedChar -> 
+                                                  encodedChar ++ encodeMessage rest encodingTable -- Add the rest of the encodings
+
+-- Takes an encoded msg and an encodingTable and returns the encoded msg
+decodeMessage :: String -> [([Char], Char)] -> Maybe String
+decodeMessage [] _ = Just []
+decodeMessage msg encodingTable = decodeMessageHelper msg encodingTable 1 ""
+
+-- A helper function for the decodeMessage function, that includes an i, that is the current decoding position.
+-- For example if i = 2 and msg = "10001", we check the first 2, i.e. "10".
+-- Additionally an accumulator for the decoded msg is added, i.e. the decodedMsg.
+decodeMessageHelper :: String -> [([Char], Char)] -> Int -> String -> Maybe String
+decodeMessageHelper [] encodingTable i [] = Nothing -- If nothing decoded, and nothing left in string -> Nothing
+decodeMessageHelper [] encodingTable i decodedMsg = Just decodedMsg -- If something decoded, and the rest of the msg is empty
+decodeMessageHelper msg encodingTable i decodedMsg = let org_text = lookup (take i msg) encodingTable
+                                                     -- See if the current path (e.g. "100") yields a char
+                                                     in
+                                                        case org_text of
+                                                          -- If not, include the next int from the path (1 or 0)
+                                                          Nothing -> decodeMessageHelper msg encodingTable (i + 1) decodedMsg
+                                                          -- Else drop the used 0s and 1s and try to decode the rest.
+                                                          -- Also add the decoded char to the decodedMsg accumulator
+                                                          Just decodedChar ->
+                                                              decodeMessageHelper (drop i msg) encodingTable 1 (decodedMsg ++ [decodedChar])
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------
+-------- HUFFMAN TREE STUFF ----------
+--------------------------------------
+
+data HTree  = Leaf Int Char
+            | Node Int [Char] HTree HTree
+            deriving (Show)
+
+-- For merging two trees, while summing up their value and strings
+mergeTrees :: HTree -> HTree -> HTree
+mergeTrees t1 t2  = Node (nodeValue t1 + nodeValue t2) (nodeChars t1 ++ nodeChars t2) t1 t2
+
+-- Get int value of a node
+nodeValue :: HTree -> Int
+nodeValue (Leaf v _) = v
+nodeValue (Node v _ _ _) = v
+
+-- Get chars from a node
+nodeChars :: HTree -> [Char]
+nodeChars (Leaf _ c) = [c]
+nodeChars (Node _ cs _ _) = cs
 
 createHuffManTree :: String -> HTree
-createHuffManTree string = let sorted_freqs = sortFrequencies (frequencies string)
-                               leaf_nodes = map (\x -> Leaf (snd x) (fst x)) sorted_freqs
+createHuffManTree string = let sortedFreqs = sortFrequencies (frequencies string) -- Char frequencies from the string input
+                               leafNodes = map (\x -> Leaf (snd x) (fst x)) sortedFreqs -- Create all leafs
                            in
-                              huffManTreeHelper leaf_nodes
+                              huffManTreeHelper leafNodes -- Use the helper to build the tree from the leaf nodes
 
 huffManTreeHelper :: [HTree] -> HTree
-huffManTreeHelper [t1] = t1
-huffManTreeHelper (first:second:rest) =
+huffManTreeHelper [] = Leaf 0 ' ' -- Dummy. If empty string, simply create a single empty node
+huffManTreeHelper [t1] = t1 -- If the list only has one element, return that element. This is the root.
+huffManTreeHelper (first:second:rest) = -- Take two first, since they are sorted, these always have the lowest value and should be used next
                                         let n = Node
                                                 ((nodeValue first) + (nodeValue second))
                                                 ((nodeChars first) ++ (nodeChars second))
                                                 first
                                                 second
                                           in
+                                             -- Add new node to the list, sort it again and rerun with the new list
                                              huffManTreeHelper (sortHuffManTreeByValue (n : rest))
 
+-- Sorts a list of Huffman trees by the node value in increasing order
 sortHuffManTreeByValue :: [HTree] -> [HTree]
 sortHuffManTreeByValue [] = []
 sortHuffManTreeByValue [t] = [t]
 sortHuffManTreeByValue lst = sortBy (\t1 t2 -> compare (nodeValue t1) (nodeValue t2)) lst
 
+-- Create the map from char to how many occurrences of that char in the string
+-- FromListWith takes a list of pairs and function to apply, if some key is present more than once
+frequencies :: String -> [(Char, Int)]
+frequencies s = toList (fromListWith (+) [(c, 1) | c <- s])
 
--- Result tree for mississippi river
+-- Sort the frequency map by most occurrences
+sortFrequencies :: [(Char, Int)] -> [(Char, Int)]
+sortFrequencies = sortBy (\ a b -> compare (snd b) (snd a))
+
+-- Result tree for mississippi river as example
 resultTree :: HTree
 resultTree = Node 17 "prmv eis" (Node 8 "prmv e"
-                                          (Node 4 "pr" (Leaf 2 'p') (Leaf 2 'r'))
+                                          (Node 4 "pr" (Leaf 2 'p')
+                                                       (Leaf 2 'r'))
                                           (Node 4 "mv e"
-                                                        (Node 2 "mv" (Leaf 1 'm') (Leaf 1 'v'))
-                                                        (Node 2 " e" (Leaf 1 ' ') (Leaf 1 'e'))))
-                                 (Node 9 "is" (Leaf 5 'i') (Leaf 4 's'))
+                                                        (Node 2 "mv" (Leaf 1 'm')
+                                                                     (Leaf 1 'v'))
+                                                        (Node 2 " e" (Leaf 1 ' ')
+                                                                     (Leaf 1 'e'))))
+                                (Node 9 "is" (Leaf 5 'i')
+                                             (Leaf 4 's'))
 
-encodeMessage :: String -> [([Int], Char)] -> String
-encodeMessage [] _ = []
-encodeMessage (char:rest) encodingTable = let pathLstAsInts = lookupByValue encodingTable char -- Path as [1, 1, 0]
-                                              pathLstAsStrings = lstOfEleToLstOfString pathLstAsInts -- Path as ["1", "1", "0"]
-                                              path = concat pathLstAsStrings -- Path as "110"
-                                          in
-                                              path ++ encodeMessage rest encodingTable -- Concat all characters
+--------------------------------------
+------ HUFFMAN TREE STUFF END --------
+--------------------------------------
 
 
-decodeMessage :: String -> [([Int], Char)] -> Maybe String
-decodeMessage [] _ = Just []
-decodeMessage msg encodingTable = decodeMessageHelper msg encodingTable 1 ""
 
-decodeMessageHelper :: String -> [([Int], Char)] -> Int -> String -> Maybe String
-decodeMessageHelper [] encodingTable i [] = Nothing -- If nothing decoded, and nothing left in string -> Nothing
-decodeMessageHelper [] encodingTable i decodedMsg = Just decodedMsg
-decodeMessageHelper msg encodingTable i decodedMsg = let intList = lstOfStringsToInt (take i msg) -- Encoding as [1, 0, 1, 0]
-                                                         org_text = lookup intList encodingTable
-                                                     in
-                                                        case org_text of
-                                                          Nothing -> decodeMessageHelper msg encodingTable (i + 1) decodedMsg
-                                                          Just decodedChar ->
-                                                              decodeMessageHelper (drop i msg) encodingTable 1 (decodedMsg ++ [decodedChar])
 
-containsKey :: Eq k => k -> [(k, v)] -> Bool
-containsKey k assocList = k `elem` [fst x | x <- assocList]
 
-lookupByValue :: Eq v => [(k, v)] -> v -> k
-lookupByValue ((key, value) : xs) lookupValue | value == lookupValue = key
+
+
+
+
+--------------------------------------
+------ ENCODING/DECODING TABLE -------
+--------------------------------------
+
+-- Make encoding / decoding table, which is a map from a binary path (e.g. "00") to a char
+makeTable :: HTree -> [([Char], Char)]
+makeTable (Leaf int char) = [("1", char)] -- If we only have a leaf the path is always just 1
+makeTable (Node int cs t1 t2) = makeTableHelper t1 "0" ++ makeTableHelper t2 "1"
+
+-- Helper function for make table helper, which includes an accumulator for the path
+makeTableHelper :: HTree -> [Char] -> [([Char], Char)]
+makeTableHelper (Leaf int char) path = [(path, char)]
+makeTableHelper (Node int cs t1 t2) path = makeTableHelper t1 (path ++ "0") ++ makeTableHelper t2 (path ++ "1")
+
+-- Lookup a in a map using a value. Returns the first key, that has the looked up value.
+-- This works, since a huffman tree has a unique path for each char, i.e. the same key cannot appear with 
+-- two different values.
+lookupByValue :: Eq v => [(k, v)] -> v -> Maybe k
+lookupByValue [] _ = Nothing
+lookupByValue ((key, value) : xs) lookupValue | value == lookupValue = Just key
                                               | otherwise = lookupByValue xs lookupValue
 
-
-lstOfEleToLstOfString :: Show a => [a] -> [String]
-lstOfEleToLstOfString = (>>= return.show)
-
-lstOfStringsToInt :: String -> [Int]
-lstOfStringsToInt str = map (read . pure :: Char -> Int) str
-
--- Huffman Coding Algorithm
--- ENCODING:
--- Calculate frequencies
--- Sort Frequencies
--- Create huffman tree (both nodes and leafs have a string text and an Int value)
--- Make table of the tree to look up paths, i.e. bit representation of a char
--- For each char look up bit representation and add to list
-
--- DECODING
--- While list of bits is not empty
-  -- Traverse tree with each bit until leaf is found
-  -- Add char at the end to new string
+------------------------------------------
+------ ENCODING/DECODING TABLE END -------
+------------------------------------------
